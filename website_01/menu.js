eee30575f6f44b14ccf96d162841d3f8dffdbec9
mayegg4869 <mayegg4869@gmail.com>
2021-08-12 02:09:40 +0900

    ver.2.00

---
+++ website_01/menu.js
@@ -0,0 +1,63 @@
+// Sticky Header
+$(window).scroll(function() {
+
+    if ($(window).scrollTop() > 1) {
+        $('.main_h').addClass('sticky');
+    } else {
+        $('.main_h').removeClass('sticky');
+    }
+});
+
+//$('.main_h').addClass('sticky');
+
+
+// Mobile Navigation
+$('.mobile-toggle').click(function() {
+    if ($('.main_h').hasClass('open-nav')) {
+        $('.main_h').removeClass('open-nav');
+    } else {
+        $('.main_h').addClass('open-nav');
+    }
+});
+
+$('.main_h li a').click(function() {
+    if ($('.main_h').hasClass('open-nav')) {
+        $('.navigation').removeClass('open-nav');
+        $('.main_h').removeClass('open-nav');
+    }
+});
+
+
+
+jQuery(function() {
+    var appear = false;
+    var pagetop = $('#page_top');
+    $(window).scroll(function () {
+      if ($(this).scrollTop() > 100) {  //100pxスクロールしたら
+        if (appear == false) {
+          appear = true;
+          pagetop.stop().animate({
+            'bottom': '10px' //下から50pxの位置に
+          }, 300); //0.3秒かけて現れる
+        }
+      } else {
+        if (appear) {
+          appear = false;
+          pagetop.stop().animate({
+            'bottom': '-50px' //下から-50pxの位置に
+          }, 300); //0.3秒かけて隠れる
+        }
+      }
+    });
+  });
+  
+  $(function(){
+    $('a[href^=#]').click(function(){
+    var speed = 500;
+    var href= $(this).attr("href");
+    var target = $(href == "#" || href == "" ? 'html' : href);
+    var position = target.offset().top - 120;
+    $("html, body").animate({scrollTop:position}, speed, "swing");
+    return false;
+    });
+   });
