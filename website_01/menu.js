8736c0de64a8c657a5789abcbc0c84b37928ddac
mayegg4869 <mayegg4869@gmail.com>
2022-06-15 09:33:19 +0900

    220615

--- website_01/menu.js
+++ website_01/menu.js
@@ -51,13 +51,3 @@ jQuery(function() {
     });
   });
   
-  $(function(){
-    $('a[href^=#]').click(function(){
-    var speed = 500;
-    var href= $(this).attr("href");
-    var target = $(href == "#" || href == "" ? 'html' : href);
-    var position = target.offset().top - 120;
-    $("html, body").animate({scrollTop:position}, speed, "swing");
-    return false;
-    });
-   });
