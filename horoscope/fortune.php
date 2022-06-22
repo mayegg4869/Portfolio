ab626fea07695be8f8f88fceacb26f3fc27bb8c1
mayegg4869 <mayegg4869@gmail.com>
2022-06-22 15:10:23 +0900

    12星座占い試作

---
+++ horoscope/fortune.php
@@ -0,0 +1,96 @@
+<?php
+//参考ドキュメント
+//https://www.nob.gr.jp/au/wordpress/1554/
+//https://qiita.com/fantm21/items/603cbabf2e78cb08133e
+
+$today = date("Y/m/d",strtotime("+12 hour"));
+$tommorow = date("Y/m/d",strtotime("+36 hour"));
+
+$url = "http://api.jugemkey.jp/api/horoscope/free/";
+$json = file_get_contents($url.$today);
+$json = mb_convert_encoding($json, 'UTF8', 'ASCII,JIS,UTF-8,EUC-JP,SJIS-WIN');
+$arr = json_decode($json,true);
+
+for ($i = 0; $i < 12; $i++) {
+    $h_sign[$i] = $arr["horoscope"][$today][$i]["sign"]; //星座
+    $h_content[$i] = $arr["horoscope"][$today][$i]["content"]; //運勢テキスト
+    $h_item[$i] = $arr["horoscope"][$today][$i]["item"];//ラッキーアイテム
+    $h_color[$i] = $arr["horoscope"][$today][$i]["color"];//ラッキーカラー
+    $h_total[$i] = $arr["horoscope"][$today][$i]["total"];//総合運
+    $h_money[$i] = $arr["horoscope"][$today][$i]["money"];//金運
+    $h_job[$i] = $arr["horoscope"][$today][$i]["job"];//仕事運
+    $h_love[$i] = $arr["horoscope"][$today][$i]["love"];//恋愛運
+    $h_rank[$i] = $arr["horoscope"][$today][$i]["rank"];//ランキング
+
+
+    //各星座にマークを割り当てる
+    switch ($h_sign[$i]) {
+        case "牡羊座":
+            $mark[$i] = "alies";
+            $h_birthday[$i] = "3/21～4/19";
+            break;
+        case "牡牛座":
+            $mark[$i] = "taurus";
+            $h_birthday[$i] = "4/20～5/20";
+            break;
+        case "双子座":
+            $mark[$i] = "gemini";
+            $h_birthday[$i] = "5/21～6/21";
+            break;
+        case "蟹座":
+            $mark[$i] = "cancer";
+            $h_birthday[$i] = "6/22～7/22";
+            break;
+        case "獅子座":
+            $mark[$i] = "leo";
+            $h_birthday[$i] = "7/23～8/22";
+            break;
+        case "乙女座":
+            $mark[$i] = "virgo";
+            $h_birthday[$i] = "8/23～9/22";
+            break;
+        case "天秤座":
+            $mark[$i] = "libra";
+            $h_birthday[$i] = "9/23～10/23";
+            break;
+        case "蠍座":
+            $mark[$i] = "scorpio";
+            $h_birthday[$i] = "10/24～11/22";
+            break;
+        case "射手座":
+            $mark[$i] = "sagittarius";
+            $h_birthday[$i] = "11/23～12/21";
+            break;
+        case "山羊座":
+            $mark[$i] = "capricorn";
+            $h_birthday[$i] = "12/22～1/19";
+            break;
+        case "水瓶座":
+            $mark[$i] = "aquarius";
+            $h_birthday[$i] = "1/20～2/18";
+            break;
+        case "魚座":
+            $mark[$i] = "pisces";
+            $h_birthday[$i] = "3/19～3/20";
+            break;
+        default:
+            $mark[$i] = "Error";
+    }
+}
+$rand = mt_rand(0, 11);
+/*
+$rand = mt_rand(0, 11);
+switch ($h_rank[$rand]) {
+    case 1:
+        $fortune = "おめでとう！　$h_sign[$rand]のあなたは運勢$h_rank[$rand]位！<br>$h_content[$rand]<br>ラッキーアイテムは$h_item[$rand]！";
+        break;
+    case 12:
+        $fortune = "ごめんなさいゴリ～。$h_sign[$rand]のあなたは運勢$h_rank[$rand]位！<br>$h_content[$rand]<br>ラッキーアイテムは$h_item[$rand]！";
+        break;
+    default:
+        $fortune = "今日の運勢$h_rank[$rand]位は$h_sign[$rand]！<br>$h_content[$rand]<br>ラッキーアイテムは$h_item[$rand]！";
+        break;
+}
+*/
+
+?>
---
+++ horoscope/index.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html lang="JP">
+<head>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0">
+<title>ゆる12星座うらない</title>
+<link rel="icon" href="favicon.ico">
+<link rel="stylesheet" href="reset.css">
+<link rel="stylesheet" href="style.css">
+    <!-- ウェブフォント -->
+    <style>
+    @import url('https://fonts.googleapis.com/css2?family=Kaisei+Decol&family=Zen+Kaku+Gothic+New&display=swap');
+    </style>
+    <!-- ウェブフォントココマデ -->
+<?php 
+include("fortune.php");
+?>
+
+</head>
+
+<body>
+<!-- ローディング画面 -->
+<div id="loader">
+    <div class="spinner">
+        <div class="double-bounce1"></div>
+        <div class="double-bounce2"></div>
+    </div>
+</div>
+
+<!-- メイン -->
+<div id="particles-js"></div>
+<div id="wrapper">
+    <h1>ゆる12星座うらない</h1>
+    <h2>yuru Horoscope</h2>
+    <h3>ゆるーいイラストで今日の運勢をお届けします。</h3>
+
+    <div  class="contents">
+        <?php
+        for ($i = 0; $i < 12; $i++) {
+            echo "<div class='horoscope'>";
+            echo "<h1>$h_sign[$i]</h1>";
+            echo "<h2>$h_birthday[$i]</h2>";
+                echo "<div class='images'>";
+                    switch ($h_rank[$i]) {
+                        case 1:
+                            echo "<img src='img/no1.png' class='ranking'>";
+                            break;
+                        case 2:
+                            echo "<img src='img/no2.png' class='ranking'>";
+                            break;
+                        case 3:
+                            echo "<img src='img/no3.png' class='ranking'>";
+                            break;
+                        default:
+                        // 何もしない
+                        ;
+                    }
+                    echo "<img src='img/$h_$mark[$i].png'>";
+                echo "</div>"; // images
+
+                echo "<p class='h_content'>$h_content[$i]</p>";
+                echo "<div class='rating'>";
+                echo "<p>ラッキーアイテム　<p class='nostyle'>$h_item[$i]</p></p>";
+                echo "<p>ラッキーカラー　<p class='nostyle'>$h_color[$i]</p></p>";
+
+                echo "<p>総合運　";
+                for ($n = 0; $n < $h_total[$i]; $n++) {
+                    echo "<img src='img/star.png' class='star'>";
+                }
+                echo "</p>金運　";
+                for ($n = 0; $n < $h_money[$i]; $n++) {
+                    echo "<img src='img/star.png' class='star'>";
+                }
+                echo "</p>仕事運　";
+                for ($n = 0; $n < $h_job[$i]; $n++) {
+                    echo "<img src='img/star.png' class='star'>";
+                }
+                echo "</p>恋愛運　";
+                for ($n = 0; $n < $h_job[$i]; $n++) {
+                    echo "<img src='img/star.png' class='star'>";
+                }
+                echo "</p>";
+                echo "</div>";
+
+            echo "</div>"; // horoscope
+        }
+
+
+
+        ?>
+
+    <!-- contents --></div>
+    <!-- フッターエリア -->
+    <footer>
+        <p>by <a href="https://mayegg4869.github.io/Portfolio/">mayegg</a></p>
+        <p><a href="https://www.ac-illust.com/">Illust AC</a> / <a href="https://www.photo-ac.com/">Photo AC</a></p>
+        <p>powerd by <a href="http://jugemkey.jp/api/">JugemKey</a>　【PR】<a href="http://www.tarim.co.jp/">原宿占い館 塔里木</a></p>
+    <!-- footer --></footer>
+<!-- wrapper --></div>
+
+<!-- JQuery 3.6.0 -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+<!-- JQueryココマデ -->
+<!-- JS -->
+<script src="js/particles.min.js"></script>
+<script src="js/script.js"></script>
+<!-- JSココマデ -->
+</body>
+</html>
