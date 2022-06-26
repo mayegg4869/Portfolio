7b52399b46a29d47c280efed70151e5c5918aa1e
mayegg4869 <mayegg4869@gmail.com>
2022-06-27 00:45:14 +0900

    ポートフォリオのリニューアル（テスト）

--- js/script.js
+++ js/script.js
@@ -1,12 +1,14 @@
-        // ランダムひとことコメント用
-        let comment = [
+// ------------------------------------------------------------
+// ランダムひとことコメント用
+        const comment = [
             "ウホウホ",
             "モグモグ",
             "ゴリラは、霊長目ヒト科ゴリラ属（ゴリラぞく、Gorilla）に分類される構成種の総称。<br>引用：Wikipedia"
             ];
-        let random = comment[Math.floor(Math.random() * comment.length)];
+        const random = comment[Math.floor(Math.random() * comment.length)];
         document.querySelector('.random').innerHTML = (random);
-    
+
+// ------------------------------------------------------------
     // fancyboxのオプション
         $(document).ready(function() {
             $('[data-fancybox]').fancybox({
@@ -15,7 +17,7 @@
                 smallBtn : true
             });
         });
-
+// ------------------------------------------------------------
         // タブ切り替え
         jQuery(function($){
             $('.tab').click(function(){
@@ -28,24 +30,63 @@
                 $('.panel').eq(index).addClass('is-show');
             });
         });
-
+// ------------------------------------------------------------
         // メニュー固定
-        var _window = $(window),
-            _header = $('.tab-group'),
-            heroBottom;
+        let win = $(window),
+            header = $('.tab-group'),
+            containBottom;
         
-        _window.on('scroll',function(){     
-            heroBottom = $('.contain').height();
-            if(_window.scrollTop() > heroBottom){
-                _header.addClass('fixed');   
+        win.on('scroll',function(){     
+            containBottom = $('.contain').height();
+            if(win.scrollTop() > containBottom){
+                header.addClass('fixed');   
             }
             else{
-                _header.removeClass('fixed');   
+                header.removeClass('fixed');   
             }
         });
-        
-        _window.trigger('scroll');
 
+        win.trigger('scroll');
+// ------------------------------------------------------------
+// キラキラエフェクト
+// https://web-dev.tech/front-end/javascript/glittering-effect-on-hover/
+
+        // glitterクラスがついた要素を全て取得
+        const glitterEls = document.querySelectorAll(".glitter");
+
+        // 取得した要素をArrayに変換
+        const glitterElsArr = Array.prototype.slice.call(glitterEls);
+
+        // 取得した要素ひとつひとつに処理を行う
+        glitterElsArr.forEach((glitterEl) => {
+        let interval;
+
+        // マウスホバー時にキラキラを生成
+        glitterEl.addEventListener("mouseenter", () => {
+            interval = setInterval(createStar.bind(undefined, glitterEl), 200);
+        });
+
+        // マウスを離すと停止
+        glitterEl.addEventListener("mouseleave", () => {
+            clearInterval(interval);
+        });
+        });
+
+        // キラキラを生成する関数
+        const createStar = (el) => {
+        const starEl = document.createElement("span");
+        starEl.className = "star";
+        starEl.style.left = Math.random() * el.clientWidth + "px";
+        starEl.style.top = Math.random() * el.clientHeight + "px";
+        el.appendChild(starEl);
+
+        // 一定時間経つとキラキラを消す
+        setTimeout(() => {
+            starEl.remove();
+        }, 1000);
+        };
+
+// ------------------------------------------------------------
         // ローディング
         window.onload = function() {
         const spinner = document.getElementById('loading');
