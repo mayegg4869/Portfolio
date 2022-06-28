b8e4b678852efff882daf489a81ac51fd4b21380
mayegg4869 <mayegg4869@gmail.com>
2022-06-28 10:10:15 +0900

    恋愛運が仕事運を参照していた不具合の修正、変数名の修正、ソース微整形

--- horoscope/fortune.php
+++ horoscope/fortune.php
@@ -25,55 +25,55 @@ for ($i = 0; $i < 12; $i++) {
     //各星座にマークと誕生日を割り当てる
     switch ($h_sign[$i]) {
         case "牡羊座":
-            $h_mark[$i] = "alies";
+            $h_symbol[$i] = "alies";
             $h_birthday[$i] = "3/21～4/19";
             break;
         case "牡牛座":
-            $h_mark[$i] = "taurus";
+            $h_symbol[$i] = "taurus";
             $h_birthday[$i] = "4/20～5/20";
             break;
         case "双子座":
-            $h_mark[$i] = "gemini";
+            $h_symbol[$i] = "gemini";
             $h_birthday[$i] = "5/21～6/21";
             break;
         case "蟹座":
-            $h_mark[$i] = "cancer";
+            $h_symbol[$i] = "cancer";
             $h_birthday[$i] = "6/22～7/22";
             break;
         case "獅子座":
-            $h_mark[$i] = "leo";
+            $h_symbol[$i] = "leo";
             $h_birthday[$i] = "7/23～8/22";
             break;
         case "乙女座":
-            $h_mark[$i] = "virgo";
+            $h_symbol[$i] = "virgo";
             $h_birthday[$i] = "8/23～9/22";
             break;
         case "天秤座":
-            $h_mark[$i] = "libra";
+            $h_symbol[$i] = "libra";
             $h_birthday[$i] = "9/23～10/23";
             break;
         case "蠍座":
-            $h_mark[$i] = "scorpio";
+            $h_symbol[$i] = "scorpio";
             $h_birthday[$i] = "10/24～11/22";
             break;
         case "射手座":
-            $h_mark[$i] = "sagittarius";
+            $h_symbol[$i] = "sagittarius";
             $h_birthday[$i] = "11/23～12/21";
             break;
         case "山羊座":
-            $h_mark[$i] = "capricorn";
+            $h_symbol[$i] = "capricorn";
             $h_birthday[$i] = "12/22～1/19";
             break;
         case "水瓶座":
-            $h_mark[$i] = "aquarius";
+            $h_symbol[$i] = "aquarius";
             $h_birthday[$i] = "1/20～2/18";
             break;
         case "魚座":
-            $h_mark[$i] = "pisces";
+            $h_symbol[$i] = "pisces";
             $h_birthday[$i] = "3/19～3/20";
             break;
         default:
-            $h_mark[$i] = "Error";
+            $h_symbol[$i] = "Error";
     }
 }
 ?>
--- horoscope/index.php
+++ horoscope/index.php
@@ -12,7 +12,6 @@
     <style>
     @import url('https://fonts.googleapis.com/css2?family=Kaisei+Decol&family=Zen+Kaku+Gothic+New&display=swap');
     </style>
-    <!-- ウェブフォントココマデ -->
 </head>
 
 <body>
@@ -42,12 +41,13 @@
             for ($i = 0; $i < 12; $i++) {
                 // コンテンツ表示エリア
                 echo "<div class='horoscope'>";
-                // 星座名
-                echo "<h1>$h_sign[$i]</h1>";
-                // 誕生日
-                echo "<h2>$h_birthday[$i]</h2>";
-                    // 1位～3位にはマークを表示
+                    // 星座名
+                    echo "<h1>$h_sign[$i]</h1>";
+                    // 誕生日
+                    echo "<h2>$h_birthday[$i]</h2>";
+                    //画像表示エリア
                     echo "<div class='images'>";
+                        // ランキング1位～3位には王冠マークを表示
                         switch ($h_rank[$i]) {
                             case 1:
                                 echo "<img src='img/no1.png' class='ranking'>";
@@ -59,44 +59,44 @@
                                 echo "<img src='img/no3.png' class='ranking'>";
                                 break;
                             default:
-                            // 3位以降は何もしない
+                                // 3位以降は何もしない
                             ;
                         }
-                        // 星座のイラストを表示
-                        echo "<img src='img/$h_mark[$i].png'>";
+                        // 星座のイメージイラストを表示
+                        echo "<img src='img/$h_symbol[$i].png'>";
                     echo "</div>"; // images
 
                     // コンテンツ本文
                     echo "<p class='h_content'>$h_content[$i]</p>";
                     
-                    // 総合運やラッキーカラーなど
+                    // 総合運やラッキーカラーのエリア
                     echo "<ul>";
-                    //運勢の5段階評価
-                    echo "<dt>総合運</dt><dd>";
-                    // 数値の数だけ★を繰り返し出す
-                    for ($n = 0; $n < $h_total[$i]; $n++) {
-                        echo "<img src='img/star.png' class='star'>";
-                    }
-                    //echo "</dd>";
-                    echo "<dt>金運</dt><dd>";
-                    for ($n = 0; $n < $h_money[$i]; $n++) {
-                        echo "<img src='img/star.png' class='star'>";
-                    }
-                    //echo "</dd>";
-                    echo "<dt>仕事運</dt><dd>";
-                    for ($n = 0; $n < $h_job[$i]; $n++) {
-                        echo "<img src='img/star.png' class='star'>";
-                    }
-                    //echo "</dd>";
-                    echo "<dt>恋愛運</dt><dd>";
-                    for ($n = 0; $n < $h_job[$i]; $n++) {
-                        echo "<img src='img/star.png' class='star'>";
-                    }
-                    echo "</dd>";
+                        //運勢の5段階評価
+                        echo "<dt>総合運</dt><dd>";  // 総合運
+                        // 数値の数だけ★を繰り返し出す
+                        for ($n = 0; $n < $h_total[$i]; $n++) {
+                            echo "<img src='img/star.png' class='star'>";
+                        }
+                        echo "</dd>";
+                        echo "<dt>金運</dt><dd>"; // 金運
+                        for ($n = 0; $n < $h_money[$i]; $n++) {
+                            echo "<img src='img/star.png' class='star'>";
+                        }
+                        echo "</dd>";
+                        echo "<dt>仕事運</dt><dd>"; // 仕事運
+                        for ($n = 0; $n < $h_job[$i]; $n++) {
+                            echo "<img src='img/star.png' class='star'>";
+                        }
+                        echo "</dd>";
+                        echo "<dt>恋愛運</dt><dd>"; // 恋愛運
+                        for ($n = 0; $n < $h_love[$i]; $n++) {
+                            echo "<img src='img/star.png' class='star'>";
+                        }
+                        echo "</dd>";
 
-                    // ラッキーカラー・アイテム
-                    echo "<dt>ラッキーアイテム</dt><dd>$h_item[$i]</dd>";
-                    echo "<dt>ラッキーカラー</dt><dd>$h_color[$i]</dd>";
+                        // ラッキーカラー・アイテム
+                        echo "<dt>ラッキーアイテム</dt><dd>$h_item[$i]</dd>";
+                        echo "<dt>ラッキーカラー</dt><dd>$h_color[$i]</dd>";
                     echo "</ul>";
 
                 echo "</div>"; // horoscope
@@ -113,10 +113,10 @@
 
 <!-- JQuery 3.6.0 -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-<!-- JQueryココマデ -->
+
 <!-- JS -->
 <script src="js/particles.min.js"></script>
 <script src="js/script.js"></script>
-<!-- JSココマデ -->
+
 </body>
 </html>
